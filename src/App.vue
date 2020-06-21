72bd31be388cea58e99a93e2aebef72c2fe76a3d
Donovon Spencer <donovon.spencer@gmail.com>
2020-06-20 20:18:11 -0400

    Rearranged the pages to look nice

--- src/App.vue
+++ src/App.vue
@@ -19,23 +19,43 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-tabs
+        v-model="tab"
+        background-color="primary darken-1"
+        dark
+        grow
+        centered
+      >
+        <v-tab>Back Story</v-tab>
+        <v-tab>Timeline</v-tab>
+        <v-tab>Home</v-tab>
+        <v-tab>Looking Forward</v-tab>
+        <v-tab>Media</v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item></v-tab-item>
+        <v-tab-item></v-tab-item>
+        <v-tab-item>
+          <Welcome/>
+        </v-tab-item>
+        <v-tab-item></v-tab-item>
+        <v-tab-item></v-tab-item>
+      </v-tabs-items>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Welcome from './components/Welcome'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Welcome,
   },
-
   data: () => ({
-    //
+    tab: 2
   }),
 };
 </script>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <v-img v-if="!showImage" src="https://cdn.gamer-network.net/2018/usgamer/obi-wan-header.jpg/EG11/thumbnail/1920x1080/format/jpg/quality/65/obi-wan-kenobi-arrives-in-star-wars-battlefront-2-in-november.jpg" contain height="300"/>
+        <v-img v-if="showImage" src="https://i.ytimg.com/vi/frszEJb0aOo/maxresdefault.jpg" contain height="300"/>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col>
+        <v-btn v-if="!showImage" @click="showImage = true" color="primary">Hello there</v-btn>
+        <v-btn v-if="showImage" @click="showImage = false" color="primary">General Kenobi</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Welcome',
+
+    data: () => ({
+      showImage: false
+    }),
+  }
+</script>
