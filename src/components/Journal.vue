370bce5b6be9c52158e11bf2582c2eb28d84f8a8
Donovon Spencer <Donovon.Spencer@gmail.com>
2021-03-26 20:06:37 -0600

    Added a second link to link to Apple Maps instead of Google. Need to check if the user is Apple and display one or the other.

--- src/components/Journal.vue
+++ src/components/Journal.vue
@@ -43,6 +43,7 @@
                 </v-card-title>
                 <v-card-subtitle>
                   <v-card :elevation="0" :href="'https://www.google.com/maps/place/' + currentEntryLocation()" target="_blank">{{ currentEntryLocation() }}</v-card>
+                  <v-card :elevation="0" :href="'http://maps.apple.com/?q=' + encodeURI(currentEntryLocation())" target="_blank">{{ currentEntryLocation() }}</v-card>
                 </v-card-subtitle>
                 <v-card-text class="white text--primary">
                   <h5>{{ currentEntryText() }}</h5>
